65af2bdbab137a59643ed85864fb20d73a3fd03f
Ruhaib Faisal <83441695+Anushka-Subasinghe@users.noreply.github.com>
2025-10-25 00:00:38 +0530

    logout functionality

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
+import api from "../api/api";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout", {}, { withCredentials: true });
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("id");
+      toast.success("Logged out successfully!");
+      navigate("/login", { replace: true });
+    } catch (error) {
+      console.error(error);
+      toast.error("Logout failed");
+    }
+  };
 
   return (
     <div className="home-container">
@@ -11,6 +26,9 @@ const HomePage = () => {
       </button>
       <button className="home-button" onClick={() => navigate("tasks")}>
         View Tasks
+      </button>
+      <button className="home-button logout" onClick={handleLogout}>
+        Logout
       </button>
     </div>
   );
